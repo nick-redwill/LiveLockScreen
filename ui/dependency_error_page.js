adb3a056d347a54e82e58680f3d2db5bd0e7a52a
redwill <hro.2016@yandex.ru>
2026-05-11 15:32:09 +0400

    fix typo

--- ui/dependency_error_page.js
+++ ui/dependency_error_page.js
@@ -17,7 +17,7 @@ class LLSDependencyErrorPage extends Adw.PreferencesPage {
             subtitle:
                 `gtk4paintablesink is not available.\n\n` +
                 `Install the GStreamer GTK4 plugin for your distribution:\n` +
-                `  • Fedora/RHEL: gstreamer1-plugins-gtk4\n` +
+                `  • Fedora/RHEL: gstreamer1-plugin-gtk4\n` +
                 `  • Ubuntu/Debian: gstreamer1.0-gtk4\n` +
                 `  • Arch: gst-plugin-gtk4\n\n` +
                 `See README.md for more information.`,
